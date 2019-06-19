48419dd32fc233265a57cd14d1faa8b538736cdd
Youri Westerman <Youri Westerman tetracon@gmail.com>
2019-06-19 14:23:27 +0200

    Formatted following project standards

--- src/Exception/UserNotFoundException.php
+++ src/Exception/UserNotFoundException.php
@@ -1,4 +1,15 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Nexylan packages.
+ *
+ * (c) Nexylan SAS <contact@nexylan.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Nexy\Slack\Exception;
 
